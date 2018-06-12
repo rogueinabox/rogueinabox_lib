7c297556cb16860d6efe2ebc72c9cd33104cf35a
Daniele Cortesi <dani.c93@hotmail.it>
2018-06-12 11:23:14 +0200

    fix random agent

--- randomagent.py
+++ randomagent.py
@@ -1,9 +1,10 @@
 from .baseagent import BaseAgent
+from .options import AgentOptions, RogueBoxOptions
 import random
 
 
 class RandomAgent(BaseAgent):
-    configuration_manager_style = "single"
+    """Implements an agent that performs random actions"""
 
     def act(self):
         actions = self.rb.get_actions()
@@ -13,14 +14,13 @@ class RandomAgent(BaseAgent):
 
 
 if __name__ == '__main__':
-    configs = {
-        'userinterface': 'tk',
-        'gui': True,
-        'gui_timer_ms': 100,
-        'state_generator': 'SingleLayer_StateGenerator',
-        'reward_generator': 'StairSeeker_13_RewardGenerator',
-        'max_step_count': 500
-    }
-
-    agent = RandomAgent(configs)
+    agent = RandomAgent(AgentOptions(
+        gui=True,
+        userinterface='tk',
+        gui_timer_ms=100,
+        roguebox_options=RogueBoxOptions(
+            state_generator='SingleLayer_StateGenerator',
+            reward_generator='StairSeeker_13_RewardGenerator',
+            max_step_count=500)
+    ))
     agent.run()
